86ac5e0fe269478a8c921bf8d79365e5202e98ac
Vitalii Voloshyn <voloshin.v.02@gmail.com>
2024-08-12 18:42:24 +0200

    feat: define blog schema

---
+++ src/content/config.ts
@@ -0,0 +1,17 @@
+import { defineCollection, z } from 'astro:content';
+
+const blog = defineCollection({
+  type: 'content',
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      image: image(),
+      alt: z.string().optional(),
+      category: z.string(),
+    }),
+});
+
+export const collections = {
+  blog,
+};
